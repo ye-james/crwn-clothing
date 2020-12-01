adab25ee1f5c9acd7453bdcd411568845cc6bb10
James <james101994@yahoo.com>
2020-11-30 18:12:23 -0800

    Added styling and effects to layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,35 +1,12 @@
 import React from 'react'
+import Directory from './Directory'
+
 import '../css/HomePage.styles.scss';
 
 const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">HATS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">JACKETS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">WOMENS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">MENS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+            <Directory />
         </div>
     );
 
