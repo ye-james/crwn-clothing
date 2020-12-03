3472e3f77559f369e9ec28ec1b36aa70e572d326
James Ye <jamesye@fb.com>
2020-12-03 15:48:39 -0800

    Update app with shop page, collections and collections item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import HomePage from './components/HomePage'
+import ShopPage from './components/ShopPage';
+
+import './App.css';
 
 const HatsPage = () => {
   return (
@@ -17,7 +20,7 @@ const App = () => {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
